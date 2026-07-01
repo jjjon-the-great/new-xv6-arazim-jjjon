1e7918451f460985175b47921ef510023448e581
jjjon-the-great <jonathan.gontmakher@gmail.com>
2026-07-01 14:29:52 +0300

    cleanup

--- notxv6/barrier.c
+++ notxv6/barrier.c
@@ -30,41 +30,17 @@ barrier()
   // Block until all threads have called barrier() and
   // then increment bstate.round.
   //
-  //pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
-  /*int waiting = 0;
+  
   pthread_mutex_lock(&bstate.barrier_mutex);
   bstate.nthread ++;
-  printf("%d\n", bstate.nthread);
   if (bstate.nthread == nthread){
-    printf("releasing?\n");
-    waiting = 1;
-    bstate.round ++;
-    bstate.nthread = 0;
-  } pthread_mutex_unlock(&bstate.barrier_mutex);
-  if (!waiting) {
-    printf("falling asleep \n");
-    pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
-    printf("released!");
-  }
-  else {
-    printf("releasing!\n");
-    pthread_cond_broadcast(&bstate.barrier_cond);
-  };*/
-  pthread_mutex_lock(&bstate.barrier_mutex);
-  bstate.nthread ++;
-  //printf("acquiring %d\n", bstate.nthread);
-  if (bstate.nthread == nthread){
-    //printf("releasing %d\n", bstate.nthread);
     bstate.round ++;
     bstate.nthread = 0;
     pthread_cond_broadcast(&bstate.barrier_cond);
     pthread_mutex_unlock(&bstate.barrier_mutex);
-    //printf("haha\n");
   } else {
-    //printf("waiting\n");
     pthread_cond_wait(&bstate.barrier_cond, &bstate.barrier_mutex);
     pthread_mutex_unlock(&bstate.barrier_mutex);
-    //printf("released\n");
   }
 }
 
